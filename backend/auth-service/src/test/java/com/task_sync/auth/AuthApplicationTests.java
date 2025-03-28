e40e4bb32e18f89e9436048a51a816d1a61d62d3
RICK <rickmartinezbanda@gmail.com>
2025-03-28 12:13:44 -0500

    auth-service

---
+++ backend/auth-service/src/test/java/com/task_sync/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_sync.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
